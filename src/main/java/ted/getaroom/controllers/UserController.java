11d73d126f081cc280e5613517b8dd9b3db54130
damik3 <d1mike@hotmail.com>
2020-08-31 17:46:51 +0300

    /api/users/{id} PutMapping implementation

--- src/main/java/ted/getaroom/controllers/UserController.java
+++ src/main/java/ted/getaroom/controllers/UserController.java
@@ -12,6 +12,7 @@ import ted.getaroom.security.jwt.JwtUtils;
 
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 //@PreAuthorize("hasRole('ADMIN')")
@@ -63,6 +64,56 @@ public class UserController {
             // If user is found and has matching IDs then allow
             if (user != null && user.getId() == id) {
                 return user;
+            }
+
+            // else return Forbidden
+            else
+                throw new ForbiddenException();
+        }
+
+
+
+        // If it is not a valid jwt token,
+        // or if it the ids are not matching,
+        else
+            throw new ForbiddenException();
+    }
+
+
+
+    @PutMapping("/{id}")
+    // Allow only users with matching IDs to access this resource
+    public User updateUser(@RequestHeader HttpHeaders headers, @RequestBody User newUser, @PathVariable Long id) {
+
+        // Get token from Authorization field
+        String jwt = headers.getFirst(HttpHeaders.AUTHORIZATION);
+
+        // Remove substring 'Bearer' from the start of the string
+        if (jwt.startsWith("Bearer"))
+            jwt = jwt.substring(7, jwt.length());
+
+
+
+        // If it is a valid jwt
+        if (jwt != null  &&  jwtUtils.validateJwtToken(jwt)) {
+
+            // Get username from jwt
+            String username = jwtUtils.getUserNameFromJwtToken(jwt);
+
+            // Load user by username
+            User user = userRepository.findByUsername(username).orElse(null);
+
+            // If user is found and has matching IDs then allow
+            if (user != null && user.getId() == id) {
+
+                // Update user
+                user.setName(newUser.getName());
+                user.setSurname(newUser.getSurname());
+                user.setEmail(newUser.getEmail());
+                user.setPhone(newUser.getPhone());
+
+                // Save and return
+                return userRepository.save(user);
             }
 
             // else return Forbidden
