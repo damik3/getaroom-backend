8ae2fc5bd624faa2312de3ecc71e310e60ed5f7b
damik3 <d1mike@hotmail.com>
2020-09-09 22:03:28 +0300

    SearchController: added CrossOrigin allow all

--- src/main/java/ted/getaroom/controllers/SearchController.java
+++ src/main/java/ted/getaroom/controllers/SearchController.java
@@ -1,9 +1,6 @@
 package ted.getaroom.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ted.getaroom.models.Reservation;
 import ted.getaroom.models.Room;
 import ted.getaroom.myExceptions.BadRequestException;
@@ -17,6 +14,7 @@ import java.util.Set;
 
 
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/search")
 public class SearchController {
