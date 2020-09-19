6744cea870c768203b3442e36514113dfaea5eaa
damik3 <d1mike@hotmail.com>
2020-09-19 19:35:37 +0300

    File upload basic implementation

--- src/main/java/ted/getaroom/GetaroomApplication.java
+++ src/main/java/ted/getaroom/GetaroomApplication.java
@@ -1,15 +1,26 @@
 package ted.getaroom;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ted.getaroom.services.FilesStorageService;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class GetaroomApplication {
+public class GetaroomApplication implements CommandLineRunner {
+
+    @Resource
+    FilesStorageService storageService;
 
     public static void main(String[] args) {
         SpringApplication.run(GetaroomApplication.class, args);
     }
 
-}
+    @Override
+    public void run(String... arg) throws Exception {
+        // storageService.deleteAll();
+        storageService.init();
+    }
 
-// Test commit
+}
